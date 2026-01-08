c8a3afada1b860df2263c6ada5d2a7beccd39482
Pietro Scurti <pietroscurti9@gmail.com>
2026-01-08 16:17:14 +0100

    prima prova card dinamica

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 
-// api
-// https://lanciweb.github.io/demo/api/pictures/
+// SELEZIONO OUTPUT DEL DOM
+const outputCard = document.querySelector(".innerCard")
+
+// RICHIAMO L'API CON UNA VARIABILE
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 
 
 
+axios.get(endpoint)
+    .then(response => {
+        const posts = response.data
+
+        let postOutputs = "";
+       
+        posts.forEach((post, i) => {
+            
+            const { title, date, url } = post;
+
+
+            postOutputs += 
+            `<div class="col-12 col-md-6 col-lg-4">
+                    <div class="card h-auto">
+                        <img src="${url}" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">${date}</p>
+                        </div>
+                    </div>
+            </div>`
+            
+            
+        });
+            
+    })
+
+    outputCard.innerHTML = postOutputs
+          
+    
+
+
+
+
+
+
+
+
+
+    
