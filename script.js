a00501462463edb9071ef69d29059fc9cd048ef6
Pietro Scurti <pietroscurti9@gmail.com>
2026-01-08 16:36:37 +0100

    card dinamiche funzionanti

--- script.js
+++ script.js
@@ -7,36 +7,38 @@ const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 
 
 
+
+// 1s
 axios.get(endpoint)
-    .then(response => {
-        const posts = response.data
-
-        let postOutputs = "";
+.then(response => {
+    const posts = response.data
+    
+    let postOutputs = "";
        
-        posts.forEach((post, i) => {
-            
-            const { title, date, url } = post;
+    posts.forEach((post, i) => {
+        
+        const { title, date, url } = post;
 
-
-            postOutputs += 
-            `<div class="col-12 col-md-6 col-lg-4">
-                    <div class="card h-auto">
-                        <img src="${url}" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <h5 class="card-title">${title}</h5>
-                            <p class="card-text">${date}</p>
-                        </div>
+        postOutputs += 
+        `<div class="col-12 col-md-6 col-lg-4">
+        <div class="card h-auto">
+                    <img src="${url}" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${date}</p>
                     </div>
-            </div>`
-            
-            
-        });
-            
-    })
+                </div>
+        </div>`
+        
+        
+    });
+    console.log(postOutputs);
+    outputCard.innerHTML = postOutputs        
+})
 
-    outputCard.innerHTML = postOutputs
           
     
+console.log('hello there!');
 
 
 
