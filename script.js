93da85f31e07175929bd3bce34d14b5cd0dd1842
Pietro Scurti <pietroscurti9@gmail.com>
2026-01-09 17:34:07 +0100

    aggiunta logica

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const outputCard = document.querySelector(".innerCard")
 
 const overlay = document.querySelector(".overlay-container");
-const button = document.querySelector(".btn-overlay");
+const overlayButton = document.querySelector(".btn-overlay");
 
 // RICHIAMO L'API CON UNA VARIABILE
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
@@ -43,33 +43,39 @@ axios.get(endpoint)
         // UNA VOLTA FINITA L'OPERAZIONE INSERISCO LA CARD NELL'HTML
         outputCard.innerHTML = postOutputs
 
-        
+
         const cards = document.querySelectorAll(".card");
 
-        // loop cards
+        // CREO UN LOOP "FOREACH" PER ITERARE LE SINGOLE CARD
 
         cards.forEach((singleCard) => {
-
+            
+            // DICHIARO LA COSTANTE IMAGE CHE EQUIVALE ALLE SINGOLE IMMAGINI PRESENTI NELLE CARD
             const image = singleCard.querySelector(".card-img-top")
 
-            // add click event to every card element
+            // AL CLICK SULLA CARD, VENGONO ESEGUITE LE SEGUENTI FUNZIONI
 
             singleCard.addEventListener('click', () => {
-                
+                // DISPLAY FLEX PER RENDERE VISIBILE L'OVERLAY
                 overlay.style.display = "flex"
-                console.log(image.src);
+
+                // DICHIARO LA COSTANTE OVERLAYIMG, CHE EQUIVALE ALL' IMG PRESENTE NELL' OVERLAYCONTAINER IN HTML 
+                // (QUELLA CHE ANDRò A CLICCARE)
                 const overlayImg = overlay.querySelector("img")
+
+                // IL SRC DEL TAG IMG PRESENTE NELL OVERLAYCONTAINER IN HTML, EQUIVALE AL SRC DELL IMG PRESENTE NELLA CARD
                 overlayImg.src = image.src
+
             });
 
 
-            
+
         });
 
     });
 
 
-button.addEventListener(`click`, () => {
+    overlayButton.addEventListener(`click`, () => {
 
     overlay.style.display = "none"
 
